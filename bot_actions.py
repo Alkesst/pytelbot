07d9d71c994e95a7473cee08d9b6962f9559f0f1
Alkesst <alec.augal@gmail.com>
2017-09-05 15:18:25 +0200

    Made some little changes in the code

--- bot_actions.py
+++ bot_actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # made with python 3
 # pylint: disable=C1001
-from telegram.ext import Updater, CommandHandler
+"""Methods for the CommandHandler"""
 
 class BotActions():
     """Makes actions with the bot"""
@@ -10,9 +10,16 @@ class BotActions():
         pass
 
     @staticmethod
-    def hola(bot, update):
-        pass
+    def start(updater):
+        """Initializes the bot"""
+        updater.message.reply_text('Hola, mundo!')
 
     @staticmethod
-    def macho(bot, update):
+    def hola(updater):
+        """Replies with a cordial salute"""
+        updater.message.reply_text('Hola, {}!'.format(updater.message.first_name))
+
+    @staticmethod
+    def macho(updater):
+        """Replies if you are altered"""
         pass
--- little_prove.py
+++ little_prove.py
@@ -3,18 +3,12 @@
 # made with python 3
 # pylint: disable=C1001
 from telegram.ext import Updater, CommandHandler
+from bot_actions import BotActions
 
 def main():
-    def start(bot, update):
-        update.message.reply_text('¡Hola, mundo!')
-
-    def hello(bot, update):
-        update.message.reply_text(
-            'Hola, {} !'.format(update.message.from_user.first_name))
-
     updater = Updater('YOUR TOKEN HERE')
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('hola', hello))
+    updater.dispatcher.add_handler(CommandHandler('start', BotActions.start))
+    updater.dispatcher.add_handler(CommandHandler('hola', BotActions.hola))
 
     updater.start_polling()
     updater.idle()
