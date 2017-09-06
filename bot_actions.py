ca3672315208d70bb64b324598e4a24af7f9d65e
Alkesst <alec.augal@gmail.com>
2017-09-06 23:39:29 +0200

    Made new methods and fixed some bugs

--- bot_actions.py
+++ bot_actions.py
@@ -4,49 +4,57 @@
 # pylint: disable=C1001
 """Methods for the CommandHandler"""
 import random
+from os import listdir
+from os.path import isfile, join
 
 
 class BotActions():
     """Makes actions with the bot"""
     @staticmethod
-    def start(updater):
+    def start(bot, update):
         """Initialize the bot"""
-        updater.message.reply_text('Hola, mundo!')
+        update.message.reply_text('Hola, mundo!')
 
     @staticmethod
-    def hola(updater):
+    def hola(bot, update):
         """Reply with a cordial salute"""
-        updater.message.reply_text('Hola, {}!'.format(updater.message.from_user.first_name))
+        update.message.reply_text('Hola, {}!'.format(update.message.from_user.first_name))
 
     @staticmethod
-    def macho(bot, updater):
+    def macho(bot, update):
         """Reply if you are altered"""
-        chat_id = updater.message.chat.id
+        chat_id = update.message.chat.id
         bot.send_audio(chat_id=chat_id, audio=open('macho.mp3', 'rb'))
 
     @staticmethod
-    def send_memes(bot, updater):
+    def send_memes(bot, update):
         """Reply with a random meme"""
-        chat_id = updater.message.chat.id
-        file_name = BotActions.random_meme_template("meme_list.txt")
+        chat_id = update.message.chat.id
+        file_name = BotActions.random_meme_template()
         bot.send_photo(chat_id=chat_id, photo=open(file_name, 'rb'))
 
     @staticmethod
-    def random_meme_template(file_name):
+    def random_meme_template():
         """Search a random meme in a list of memes"""
-        num_lines = sum(1 for line in open(file_name, 'r'))
-        random_file_pos = int(random.random()*num_lines)
-        file_opened = open(file_name, 'r')
-        linea = 0
-        has_next = True
-        while has_next and linea < random_file_pos:
-            line = file_opened.readline()
-            linea += 1
-            if not line:
-                has_next = False
-        return file_opened.readline()
+        onlyfiles = [f for f in listdir('/Users/alec/Desktop/Memes') if isfile(join('/Users/alec/Desktop/Memes', f)) and f != '.DS_Store']
+        lines = len(onlyfiles)
+        random_meme = int(round(random.random()*lines, 0))
+        return '/Users/alec/Desktop/Memes/' + onlyfiles[random_meme]
 
     @staticmethod
-    def ping(bot, updater):
+    def ping(bot, update):
         """Reply with a pong."""
-        bot.send_message(chat_id=updater.message.chat.id, "Pong!")
+        bot.send_message(chat_id=update.message.chat.id, text="Pong!")
+
+    @staticmethod
+    def id(bot, update):
+        chat_id = update.message.chat.id
+        bot.send_message(chat_id=chat_id, text='`' + str(update.message.from_user.id) + '`', reply_to_message_id=update.message.message_id, parse_mode='Markdown')
+
+    @staticmethod
+    def show_error(bot, update, error):
+        raise error
+
+    @staticmethod
+    def help(bot, update):
+        pass
--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from bot_actions import BotActions
 
 
 def main():
-    updater = Updater('YOUR TOKEN HERE')
+    updater = Updater('TOKENSITO')
     updater.dispatcher.add_handler(CommandHandler('start', BotActions.start))
     updater.dispatcher.add_handler(CommandHandler('hola', BotActions.hola))
     updater.dispatcher.add_handler(CommandHandler('macho', BotActions.macho))
-    updater.dispatcher.add_handler(CommandHandler('nudes', BotActions.sned_memes))
+    updater.dispatcher.add_handler(CommandHandler('nudes', BotActions.send_memes))
     updater.dispatcher.add_handler(CommandHandler('ping', BotActions.ping))
-
+    updater.dispatcher.add_handler(CommandHandler('id', BotActions.id))
+    updater.dispatcher.add_error_handler(BotActions.show_error)
     updater.start_polling()
     updater.idle()
 
