f51df8fc5ed3382a1f583d831421cdf11b544380
Alkesst <alec.augal@gmail.com>
2017-09-18 14:45:53 +0200

    Haciendo pruebas...

--- bot_actions.py
+++ bot_actions.py
@@ -52,7 +52,7 @@ class BotActions():
 
     @staticmethod
     def prueba(bot, update):
-        bot.send_message(chat_id=update.message.chat.id, text=update.message.text)
+        bot.send_message(chat_id=update.message.chat.id, text=str(type(update.message.text)))
 
     @staticmethod
     def id_user(bot, update):
